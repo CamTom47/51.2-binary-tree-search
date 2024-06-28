b0cfa18b5cd5747d89ec472cf00660576d24df9b
Cameron Thomas <camthomas47@gmail.com>
2024-06-27 22:11:15 -0400

    BST functionalities completed

--- dsa-bsts/binary-search-tree.js
+++ dsa-bsts/binary-search-tree.js
@@ -15,27 +15,117 @@ class BinarySearchTree {
    * Returns the tree. Uses iteration. */
 
   insert(val) {
+    
+    
+    //If the BST is empty. Set root node as newNode and return.
+     if(!this.root){
+       this.root = new Node(val);
+       return this
+     }
+     
+    let currentNode = this.root;
+     
 
+    //where there is a current node, compare currentNode against newnode
+    while(currentNode){
+      // if newNode is less than currentNode and there is a currentNode.left. currentNode pointer to be currentNode.left
+      if(val < currentNode.val){
+        if(currentNode.left){
+          currentNode = currentNode.left
+        } else {
+        currentNode.left = new Node(val)
+        return this;
+      }}
+
+      // if newNode is greater than currentNode and there is a currentNode.right. currentNode pointer to be currentNode.right
+      else if(val > currentNode.val){
+        if(currentNode.right){
+          currentNode = currentNode.right
+        } else {
+          currentNode.right = new Node(val)
+          return this
+        }
+      }
+      
+    }  
   }
+
+//   
 
   /** insertRecursively(val): insert a new node into the BST with value val.
    * Returns the tree. Uses recursion. */
 
-  insertRecursively(val) {
+  insertRecursively(val, current = this.root) {
+    if(!this.root){
+      this.root = new Node(val);
+      return this
+    }
 
-  }
+     //where there is a current node, compare currentNode against newnode
+      if(val < current.val){
+        if(!current.left){
+          current.left = new Node(val);
+          return this;
+        } 
+        return this.insertRecursively(val, current.left);
+      }
+      else {
+        if(!current.right){
+          current.right = new Node(val);
+          return this;
+        }
+        return this.insertRecursively(val, current.right)
+
+      }
+    }  
 
   /** find(val): search the tree for a node with value val.
    * return the node, if found; else undefined. Uses iteration. */
 
   find(val) {
+    
+      //If the BST is empty. Set root node as newNode and return.
+      if(!this.root){
+        this.root = newNode
+      }
+      
+     let currentNode = this.root;
+
+     // check first node value against currentNode value and pick a left or right as new pointer.
+
+     while(currentNode){
+
+      if(currentNode.val === val ) return currentNode
+      else if(val < currentNode.val){
+        currentNode = currentNode.left
+      }
+      else{
+        currentNode = currentNode.right
+      }
+     }
+
+     return undefined
 
   }
 
   /** findRecursively(val): search the tree for a node with value val.
    * return the node, if found; else undefined. Uses recursion. */
 
-  findRecursively(val) {
+  findRecursively(val, current=this.root) {
+    if(!this.root){
+      return undefined
+    }
+
+    if(val < current.val){
+      if(current.left === null) return undefined;
+      return this.findRecursively(val, current.left);
+    }
+    else if(val > current.val){
+      if(current.right === null) return undefined
+      return this.findRecursively(val, current.right);
+    }
+
+    return current
 
   }
 
@@ -44,6 +134,27 @@ class BinarySearchTree {
 
   dfsPreOrder() {
 
+      //If the BST is empty. Set root node as newNode and return.
+      if(!this.root){
+        return undefined
+      }
+      
+    let visitedNodes = [];
+    let currentNode = this.root;
+     
+
+     const traverse = (node) => {
+      
+       visitedNodes.push(node.val); 
+       node.left && traverse(node.left);
+       node.right && traverse(node.right);
+       
+     }
+
+     traverse(currentNode);
+     return visitedNodes
+
+
   }
 
   /** dfsInOrder(): Traverse the array using in-order DFS.
@@ -51,12 +162,51 @@ class BinarySearchTree {
 
   dfsInOrder() {
 
+     //If the BST is empty. Set root node as newNode and return.
+     if(!this.root){
+      return undefined
+    }
+    
+  let visitedNodes = [];
+  let currentNode = this.root;
+   
+
+   const traverse = (node) => {
+    
+     node.left && traverse(node.left);
+     visitedNodes.push(node.val); 
+     node.right && traverse(node.right);
+     
+   }
+
+   traverse(currentNode);
+   return visitedNodes
+
   }
 
   /** dfsPostOrder(): Traverse the array using post-order DFS.
    * Return an array of visited nodes. */
 
   dfsPostOrder() {
+     //If the BST is empty. Set root node as newNode and return.
+     if(!this.root){
+      return undefined
+    }
+    
+  let visitedNodes = [];
+  let currentNode = this.root;
+   
+
+   const traverse = (node) => {
+    
+     node.left && traverse(node.left);
+     node.right && traverse(node.right);
+     visitedNodes.push(node.val); 
+     
+   }
+
+   traverse(currentNode);
+   return visitedNodes
 
   }
 
@@ -64,6 +214,32 @@ class BinarySearchTree {
    * Return an array of visited nodes. */
 
   bfs() {
+
+    //If the BST is empty. Set root node as newNode and return.
+    if(!this.root){
+      return undefined
+    }
+    
+  let visitedNodes = [];
+  let current = this.root;
+  let queue = [];
+
+  queue.push(current)
+   
+    while (queue.length){
+      current = queue.shift();
+      visitedNodes.push(current.val);
+      if (current.left){
+        queue.push(current.left);
+      }
+      if (current.right){
+        queue.push(current.right);
+      }
+
+      
+    }
+
+   return visitedNodes
 
   }
 
